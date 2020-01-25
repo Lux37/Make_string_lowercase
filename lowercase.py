160576c633582de0485afcedba9b0bdf63d05525
Luca <lrazov7@gmail.com>
2020-01-25 16:48:43 +0100

    Initial commit

---
+++ lowercase.py
@@ -0,0 +1,14 @@
+print("Hy! We can make a string lowercase.")
+
+# Python code for implementation of upper()
+
+# checking for uppercase characters
+str_one = "Today"
+str_two = "Is"
+str_three = "A"
+str_four = "BeautiFul"
+str_five = "DAY"
+print(str_one + " " + str_two + " " + str_three + " " + str_four + " " + str_five)
+
+string = 'TODAY IS A BEAUTIFUL DAY'
+print(string.lower())
